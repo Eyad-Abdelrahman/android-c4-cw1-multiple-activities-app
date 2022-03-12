454abb46e627ae72d4624ee0f7010bed22d0263e
Eyad-Abdelrahman <eyad.abdelrahman.yahia@gmail.com>
2022-03-12 11:36:29 +0300

    done

--- name_age_D4/app/src/main/java/com/example/name_age/MainActivity.java
+++ name_age_D4/app/src/main/java/com/example/name_age/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.name_age;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button go = findViewById(R.id.button2);
+        EditText name = findViewById(R.id.editTextTextPersonName2);
+        EditText age = findViewById(R.id.editTextTextPersonName);
+
+
+
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,NewMainActivity2.class);
+                startActivity(i);
+            }
+        });
     }
 }
